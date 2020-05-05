fa49f416a0afd65ac13df1ed715e71ff61b950ba
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-05-05 13:23:44 +0200

    Add styling to blog component

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,6 +1,18 @@
 import React from 'react'
+const blogStyle = {
+  paddingTop: 10,
+  paddingLeft: 10,
+  paddingBottom: 10,
+  paddingRight: 10,
+  border: 'solid',
+  borderWidth: 1,
+  marginBottom: 10,
+  marginLeft: 20,
+  marginRight: 20
+}
+
 const Blog = ({ blog }) => (
-  <div>
+  <div style={blogStyle}>
     {blog.title} {blog.author}
   </div>
 )
