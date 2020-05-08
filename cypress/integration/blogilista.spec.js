90ca615dbaa9e16ae8c5ae95ff9ebc5c27d8f2ff
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-05-08 10:56:42 +0200

    Finish 5.18

--- cypress/integration/blogilista.spec.js
+++ cypress/integration/blogilista.spec.js
@@ -1,6 +1,13 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Tommi',
+      username: 'Tsaarika',
+      password: 'tosiSALAinen'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
+
     cy.visit('http://localhost:3000')
   })
 
@@ -9,5 +16,23 @@ describe('Blog app', function() {
     cy.contains('username')
     cy.contains('password')
     cy.get('button').contains('login')
+  })
+
+  describe('login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('Tsaarika')
+      cy.get('#password').type('tosiSALAinen')
+      cy.get('#login-button').click()
+  
+      cy.contains('Tommi logged in')
+    })
+
+    it('fails with wrong password', function() {
+      cy.get('#username').type('Tsaarika')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+  
+      cy.contains('Wrong credentials')
+    })
   })
 })
