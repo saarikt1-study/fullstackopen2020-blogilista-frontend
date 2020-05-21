bb1b355c2e78d0d54dc6963bc54c0e07603ff5eb
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-05-21 10:04:23 +0200

    Finish 7.14

--- src/components/User.js
+++ src/components/User.js
@@ -13,9 +13,13 @@ const User = () => {
 
   return (
     <div>
-      <h2>
-        {user.name}
-      </h2>
+      <h2>{user.name}</h2>
+      <h3>Added blogs:</h3>
+      <ul>
+        {user.blogs.map(blog => 
+          <li key={blog.id}>{blog.title}</li>
+        )}
+      </ul>
     </div>
   )
 }
